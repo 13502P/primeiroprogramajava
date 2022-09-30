8db879ab76c3cf78ba36c0bca0254ceea77192b6
primeiro <pedro.nabais.silva@gmail.com>
2022-09-30 12:41:14 +0100

    segundo commit

---
+++ src/exnome.java
@@ -0,0 +1,11 @@
+public class exnome {
+    public static void main(String[] args) {
+        System.out.println("\n+-----+"
+                           + "\n-pedro-"
+                           + "\n+-----+");
+        System.out.println("+-----+");
+        System.out.println("-Pedro-");
+        System.out.println("+-----+");
+        System.out.println("\n+-----+\n-Pedro\n+-----+");
+    }
+}
